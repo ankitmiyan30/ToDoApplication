7d3d0abc63f96a5ae0a542125b99afca56e92111
https://github.com/ <ankitmiyan30@gmail.com>
2020-10-20 10:23:22 +0530

    inital app

---
+++ ToDoApp/ToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Anthony Ankit on 20/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
